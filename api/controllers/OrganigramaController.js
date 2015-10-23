53306d88a03697b7dd9b3a05fb750865902343a9
f_fischer <francisco.fischer92@gmail.com>
2015-10-23 16:25:58 +0000

    Alta, baja y modificai'on de empleado.

---
+++ api/controllers/OrganigramaController.js
@@ -0,0 +1,11 @@
+/**
+ * OrganigramaController
+ *
+ * @description :: Server-side logic for managing organigramas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ assets/js/app.js
@@ -0,0 +1,60 @@
+var app = angular.module("sge",[]);
+app.controller("OrganigramaController", function($scope,$http){
+    $scope.organigrama = [];
+    $scope.addItem = function () {
+        
+    }
+    
+    var crearOrganigrama = function () {
+        var options = {};
+        //Items
+        var listaEmpleados = obtenerEmpleados();
+        
+        
+
+        
+    };
+    var obtenerEmpleados = function(){
+        //app.controller.EmpleadoController.listarEmpleados
+        $http.get("/empleados/").then(function(results){
+            return $scope.epleados = results.data;
+        });
+        
+    };
+});
+app.controller("EmpleadoController", function($scope,$http){
+    $scope.empleados = [];
+    
+    
+     var listarEmpleados = function () {
+        var promesaEmpleados = $http.get("/empleado/");
+        promesaEmpleados.then(function(results){
+            $scope.empleados = results.data;
+        });
+    };
+    
+    $scope.empleado = {
+        nombre:""
+    };
+    
+    $scope.altaEmpleado = function () {
+        $http.post("/empleado/",$scope.empleado).then(listarEmpleados);
+    };
+    
+    $scope.bajaEmpleado = function (id) {
+        $http.delete("/empleado/"+id).then(listarEmpleados);
+    };
+    
+    $scope.modificarEmpleado = function (id) {
+        var empleadoAModificar = $http.get("/empleado/"+id);
+        $http.post("/empleado/"+id,$scope.empleado).then(listarEmpleados);
+    };
+    
+     var buscarEmpleado = function (id) {
+        var promesaEmpleadoAModificar = $http.get("/empleado/"+id);
+        promesaEmpleadoAModificar.then(function(results){
+            $scope.empleados = results.data;
+        });
+    };
+    
+});
